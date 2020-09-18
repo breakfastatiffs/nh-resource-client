fcd0ade45c54a9bc6119cb4627b438b44cee08f1
Tiff Joy <tiffanysummerford@gmail.com>
2020-09-18 17:47:36 -0400

    fixed header test by wrapping it with a browser router

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Header from '../components/Header/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders Header without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDom.render(<Header />, div);
+  ReactDom.render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>, 
+  div);
 
   ReactDom.unmountComponentAtNode(div);
 });
